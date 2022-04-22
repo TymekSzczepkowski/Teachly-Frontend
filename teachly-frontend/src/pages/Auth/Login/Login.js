d41cae71d2cd5fb9e1f0c181658b80d012c300cc
Tymoteusz <mekowskytymek@gmail.com>
2022-04-22 19:53:40 +0200

    login and registration setup

---
+++ teachly-frontend/src/pages/Auth/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <form>
+        <h1>Zaloguj się</h1>
+        <div>
+          <input placeholder="Podaj email" type="email" ></input>
+        </div>
+        <div>
+          <input placeholder="Podaj hasło" type="password"></input>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ teachly-frontend/src/pages/Auth/Register/Register.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Zarejestruj się</h1>
+        </div>
+        <div>
+          <input placeholder='Podaj email'></input>
+        </div>
+        <div>
+          <button>Dalej</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
